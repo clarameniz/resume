84f0f65e2c2988626386919add2ebcb5f083bb7f
Clara Meniz <clarameniz@gmail.com>
2020-05-26 18:25:27 +0200

    Updates

---
+++ precache-manifest.9e92551d3b598a2fc139162e7ac0b4eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38145707a111b905db192656bd43918",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "0b918107fd4bfce3c181",
+    "url": "/resume/static/js/2.4e9eae04.chunk.js"
+  },
+  {
+    "revision": "4386012a6d334684338618496b7aac20",
+    "url": "/resume/static/js/2.4e9eae04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b31652296c0faaa2a1",
+    "url": "/resume/static/js/main.24bdc472.chunk.js"
+  },
+  {
+    "revision": "e0f7dc91a34c7427a62d",
+    "url": "/resume/static/js/runtime-main.a54ac2ac.js"
+  }
+]);
